18c8822e4c28e0daea697c97276e014ae104e6d5
Derrick Nyasio <dnyasio2014@gmail.com>
2022-09-16 14:55:59 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(names, string) {
+    let list = [];
+    for (let name of names) {
+        if (name === string) {
+            list.push(name);
+        }
+    }
+    return list;
+}
+
+function fuzzyMatch(drivers, string) {
+    let list = [];
+    for (let driver of drivers) {
+        if (driver[0] === string[0]) {
+            list.push(driver);
+        }
+    }
+    return list;
+}
+
+function matchName(drivers, string) {
+    let list = [];
+    for (let driver of drivers) {
+        if (driver.name === string) {
+            list.push(driver);
+        }
+    }
+    return list;
+}
